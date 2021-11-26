d3fc926158de96d6f36df98dcfd175eee0ce2887
Jeferson Quaiato <jefersonquaiato@gmail.com>
2021-11-26 18:17:23 -0300

    Criação da pasta SQL com o create table e insert

---
+++ ControleDeEstoque/SQL/CreateTabela.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Produtos] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]       NVARCHAR (MAX) NOT NULL,
+    [Codigo]     FLOAT (53)     NOT NULL,
+    [Quantidade] FLOAT (53)     NOT NULL,
+    CONSTRAINT [PK_Produtos] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
---
+++ ControleDeEstoque/SQL/Insert.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT [dbo].[Produtos] ON
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (1, N'Caderno', 3001, 15)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (2, N'Camiseta', 3002, 15)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (3, N'Lapis', 3003, 21)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (10, N'Headset', 3004, 8)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (1008, N'Grand Theft Auto 5', 3005, 12)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (1009, N'Caneta', 3009, 10)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (1010, N'Teclado', 3008, 15)
+INSERT INTO [dbo].[Produtos] ([Id], [Nome], [Codigo], [Quantidade]) VALUES (1011, N'Mouse', 3006, 5)
+SET IDENTITY_INSERT [dbo].[Produtos] OFF
